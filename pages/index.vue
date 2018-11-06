d801bafec0db10646d17cbe7212388835c481162
njzy <1120897196@qq.com>
2018-11-06 17:56:35 +0800

    refactor: 首页和博文详情页改为从后台获取数据

--- pages/index.vue
+++ pages/index.vue
@@ -2,304 +2,37 @@
   <v-layout row wrap>
     <v-flex md9>
       <v-layout row wrap>
-        <v-flex md12>
-          <v-card hover to='/blogs/1'>
-            <v-img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" aspect-ratio="2.75">
+        <v-flex v-for="(blog,index) in blogs" :key="index" :class="{md6:isMd6(index)}">
+          <v-card hover :to="'/blogs/'+blog._id">
+            <v-img v-if="blog.cover" :src="blog.cover">
             </v-img>
             <v-card-title primary-title class="pb-1">
               <v-layout column>
                 <v-flex>
                   <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
+                    <span>{{blog.createTime}}</span> | <span>{{blog.author.name}}</span>
                   </div>
                 </v-flex>
                 <v-flex>
-                  <h3 class="title">如何给捷径添加自动运行请求</h3>
+                  <h3 class="title">{{blog.title}}</h3>
                 </v-flex>
                 <v-flex>
-                  <div>捷径正式推出以后我在微博收到了很多咨询，发现大家有很多需求都是捷径本身并不支持的，其中包括在指定的时间或地点自动运行预先设置好的捷径</div>
+                  <div>{{blog.content}}</div>
                 </v-flex>
               </v-layout>
             </v-card-title>
             <v-card-actions class="pt-0">
               <v-btn-toggle>
                 <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
+                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">{{blog.views}}</span>
                 </v-btn>
                 <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
+                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">{{blog.likes.length}}</span>
                 </v-btn>
               </v-btn-toggle>
               <v-spacer></v-spacer>
               <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md6>
-          <v-card hover>
-            <v-card-title primary-title class="pb-1">
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title blog-title">Pixel 3 个人使用报告：一部更有活力更年轻的手机</h3>
-                </v-flex>
-                <v-flex>
-                  <div class="overview">如果你错过了发布会，可以先去看一下 Google Pixel 3 的宣传视频合集，了解一下在 Google 眼中今年 Pixel 的亮点。值得一提的是，这几部视频里的美学表达、文字排版和运用都值得有兴趣的同学学习一下。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md6>
-          <v-card hover>
-            <v-card-title primary-title class="pb-1">
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title blog-title">Pixel 3 个人使用报告：一部更有活力更年轻的手机</h3>
-                </v-flex>
-                <v-flex>
-                  <div class="overview">如果你错过了发布会，可以先去看一下 Google Pixel 3 的宣传视频合集，了解一下在 Google 眼中今年 Pixel 的亮点。值得一提的是，这几部视频里的美学表达、文字排版和运用都值得有兴趣的同学学习一下。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md12>
-          <v-card hover>
-            <v-img src="https://cdn.sspai.com/2018/10/27/e0112cfe0f17ecc44ca92ae7cc4adfbd.png?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" aspect-ratio="2.75"></v-img>
-            <v-card-title primary-title class="pb-1">
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title">Pixel 3 个人使用报告：一部更有活力更年轻的手机</h3>
-                </v-flex>
-                <v-flex>
-                  <div>如果你错过了发布会，可以先去看一下 Google Pixel 3 的宣传视频合集，了解一下在 Google 眼中今年 Pixel 的亮点。值得一提的是，这几部视频里的美学表达、文字排版和运用都值得有兴趣的同学学习一下。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md12>
-          <v-card hover>
-            <v-img src="https://cdn.sspai.com/article/e69ac3b6-99a9-b802-d219-a59aff230882.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" aspect-ratio="2.75"></v-img>
-            <v-card-title primary-title>
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title">每天对着屏幕进行专业工作的你，需要更好一点的显示器：BenQ PD2700U</h3>
-                </v-flex>
-                <v-flex>
-                  <div>屏幕大一圈就能多显示几行报表；一块可纵向显示的屏幕能减轻 Debug 的痛苦；更广的色域总能避免修图时色差的尴尬。毋庸置疑的是，对相当一部分人来说，显示器的优劣与生产力直接挂钩。不同的人群对显示器的需求方向也不尽相同。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md6>
-          <v-card hover>
-            <v-card-title primary-title class="pb-1">
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title blog-title">Pixel 3 个人使用报告：一部更有活力更年轻的手机</h3>
-                </v-flex>
-                <v-flex>
-                  <div class="overview">如果你错过了发布会，可以先去看一下 Google Pixel 3 的宣传视频合集，了解一下在 Google 眼中今年 Pixel 的亮点。值得一提的是，这几部视频里的美学表达、文字排版和运用都值得有兴趣的同学学习一下。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md6>
-          <v-card hover>
-            <v-card-title primary-title class="pb-1">
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title blog-title">Pixel 3 个人使用报告：一部更有活力更年轻的手机</h3>
-                </v-flex>
-                <v-flex>
-                  <div class="overview">如果你错过了发布会，可以先去看一下 Google Pixel 3 的宣传视频合集，了解一下在 Google 眼中今年 Pixel 的亮点。值得一提的是，这几部视频里的美学表达、文字排版和运用都值得有兴趣的同学学习一下。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md12>
-          <v-card hover>
-            <v-img src="https://cdn.sspai.com/article/bf36fbde-bbac-9359-d47d-dfac2dee4549.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" aspect-ratio="2.75"></v-img>
-            <v-card-title primary-title>
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title">印象笔记入门进阶与避坑指南</h3>
-                </v-flex>
-                <v-flex>
-                  <div>大学期间，每到考试周倒计时我的书包里就会多出很多笔记本，因为自己从小写字潦草所以经常会出现隔几天就认不出之前写了什么的尴尬；考研备战，我还不幸有过丢失重要复习笔记的心酸经历。字迹潦草加上笔记本买买丢丢是我最初产生把纸质笔记转变为电子笔记的动力。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
-              </v-avatar>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-        <v-flex md12>
-          <v-card hover>
-            <v-card-title primary-title class="pb-1">
-              <v-layout column>
-                <v-flex>
-                  <div class="body-1 font-weight-thin">
-                    <span>2018-09-25</span> | <span>admin</span>
-                  </div>
-                </v-flex>
-                <v-flex>
-                  <h3 class="title">Pixel 3 个人使用报告：一部更有活力更年轻的手机</h3>
-                </v-flex>
-                <v-flex>
-                  <div>如果你错过了发布会，可以先去看一下 Google Pixel 3 的宣传视频合集，了解一下在 Google 眼中今年 Pixel 的亮点。值得一提的是，这几部视频里的美学表达、文字排版和运用都值得有兴趣的同学学习一下。</div>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-            <v-card-actions class="pt-0">
-              <v-btn-toggle>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-eye</v-icon><span class="ml-1">1</span>
-                </v-btn>
-                <v-btn flat disabled class="mx-0">
-                  <v-icon size="10">mdi-thumb-up</v-icon><span class="ml-1">99</span>
-                </v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-              <v-avatar :size="20">
-                <img src="https://cdn.sspai.com/article/b73981b9-2abd-1aa7-5694-288bab62a443.jpg?imageMogr2/quality/95/thumbnail/!700x233r/gravity/Center/crop/700x233" alt="avatar">
+                <img :src="blog.author.avatar" alt="avatar">
               </v-avatar>
             </v-card-actions>
           </v-card>
@@ -342,23 +75,22 @@ export default {
     async getAllBlogs() {
       let res = await this.$axios.$get('/blogs')
       this.blogs = res.data
+    },
+    isMd6(index) {
+      if (
+        !this.blogs[index].cover &&
+        (!this.blogs[index + 1].cover || index + 1 != this.blogs.length)
+      ) {
+        return true
+      } else {
+        return false
+      }
     }
   }
 }
 </script>
 
-<style lang="scss">
-// .right-content .v-card {
-//   .v-card__title,
-//   .v-card__text {
-//     padding: 8px 16px;
-//     .v-btn {
-//       min-width: 40px;
-//     }
-//   }
-// }
-</style>
-<style>
+<style lang="scss" scoped>
 .blog-title {
   height: 40px;
   overflow: hidden;
