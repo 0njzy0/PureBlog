28e41ae907651b1744455be8392ebe4dc7a3dcf0
njzy <1120897196@qq.com>
2018-11-07 20:34:15 +0800

    docs: 引入dayjs插件

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/vuetify'],
+  plugins: ['~/plugins/vuetify', '~/plugins/dayjs'],
 
   /*
    ** Nuxt.js modules
---
+++ plugins/dayjs.js
@@ -0,0 +1,28 @@
+import dayjs from 'dayjs'
+import Vue from 'vue';
+
+function formatTime(time) {
+  let minute = dayjs().diff(time, 'minute')
+  let hour = dayjs().diff(time, 'hour')
+  let day = dayjs().diff(time, 'day')
+  let week = dayjs().diff(time, 'week')
+  let month = dayjs().diff(time, 'month')
+  if (minute == 0) {
+    return '刚刚'
+  } else if (minute < 60) {
+    return minute + '分钟前'
+  } else if (hour < 24) {
+    return hour + '小时前'
+  } else if (day < 7) {
+    return day + '天前'
+  } else if (week < 5) {
+    return week + '周前'
+  } else if (month < 12) {
+    return month + '月前'
+  } else {
+    return dayjs(time).format('YYYY-MM-DD')
+  }
+}
+
+Vue.prototype.$dayjs = dayjs
+Vue.prototype.$formatTime = formatTime
