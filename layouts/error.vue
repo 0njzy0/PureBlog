143035622c20562a5cb1b38f90b68e40a26907b5
njzy <1120897196@qq.com>
2018-11-07 13:21:13 +0800

    style: 显示具体错误信息

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,6 +3,7 @@
     <v-flex md12 style="text-align:center">
       <h1 class="display-3 primary--text mb-4" v-if="error.statusCode === 404">页面不存在</h1>
       <h1 v-else>应用发生错误异常</h1>
+      <p>{{error.message}}</p>
       <v-btn to='/'>首页</v-btn>
     </v-flex>
   </v-layout>
