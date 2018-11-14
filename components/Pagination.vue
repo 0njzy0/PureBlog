0c35d2b01a6d2f759bd22be7e91a5d428ab4e0e5
njzy <1120897196@qq.com>
2018-11-14 15:13:36 +0800

    refactor: 单独封装分页组件

---
+++ components/Pagination.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-pagination :value="page" :length="length" :total-visible="7" @input="handlePage"></v-pagination>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: ['path', 'page', 'limit', 'total'],
+  computed: {
+    length() {
+      if (this.limit >= this.total) {
+        return 1
+      } else {
+        if (this.total % this.limit == 0) {
+          return parseInt(this.total / this.limit)
+        } else {
+          return parseInt(this.total / this.limit + 1)
+        }
+      }
+    }
+  },
+  methods: {
+    handlePage(page) {
+      this.$router.push({ path: this.path, query: { page } })
+    }
+  }
+}
+</script>
