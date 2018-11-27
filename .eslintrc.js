170252143485063b818c973ca8d2dc275fe3b27c
njzy <1120897196@qq.com>
2018-11-27 14:02:26 +0800

    feat: 利用eslint+prettier进行代码检查和格式化

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+    'prettier',
+    'prettier/standard',
+    'prettier/vue'
+  ],
+  plugins: ['standard', 'vue', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { semi: false, singleQuote: true }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any'
+        }
+      }
+    ],
+    eqeqeq: ['off']
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    es6: true,
+    node: true
+  }
+}
