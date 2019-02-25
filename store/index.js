75d09529babcd3cff3cab547d650ad3ec11642c3
njzy <1120897196@qq.com>
2019-02-25 20:25:11 +0800

    refactor: store mode改为module

--- store/index.js
+++ store/index.js
@@ -1,44 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+export const state = () => ({
+  tags: [],
+  categories: []
+})
 
-Vue.use(Vuex)
+export const mutations = {
+  setTagsAndCategories(state, payload) {
+    state.tags = payload.tags
+    state.categories = payload.categories
+  }
+}
 
-const store = () =>
-  new Vuex.Store({
-    state: {
-      tags: [],
-      categories: []
-    },
-    mutations: {
-      setTagsAndCategories(state, payload) {
-        state.tags = payload.tags
-        state.categories = payload.categories
-      }
-    },
-    actions: {
-      async nuxtServerInit({ dispatch }, { app, error }) {
-        await dispatch('loadTagsAndCategories', {
-          app,
-          error
-        })
-      },
-      async loadTagsAndCategories({ commit }, { app, error }) {
-        try {
-          let tagsPromise = app.$axios.$get('/tags')
-          let categoriesPromise = app.$axios.$get('/categories')
-          const [tags, categories] = await Promise.all([tagsPromise, categoriesPromise])
-          commit('setTagsAndCategories', {
-            tags: tags.data,
-            categories: categories.data
-          })
-        } catch (e) {
-          error({
-            statusCode: 500,
-            message: `获取标签和类别数据失败:${e.message}`
-          })
-        }
-      }
+export const actions = {
+  async nuxtServerInit({ dispatch }, { app, error }) {
+    await dispatch('loadTagsAndCategories', {
+      app,
+      error
+    })
+  },
+  async loadTagsAndCategories({ commit }, { app, error }) {
+    try {
+      let tagsPromise = app.$axios.$get('/tags')
+      let categoriesPromise = app.$axios.$get('/categories')
+      const [tags, categories] = await Promise.all([tagsPromise, categoriesPromise])
+      commit('setTagsAndCategories', {
+        tags: tags.data,
+        categories: categories.data
+      })
+    } catch (e) {
+      error({
+        statusCode: 500,
+        message: `获取标签和类别数据失败:${e.message}`
+      })
     }
-  })
-
-export default store
+  }
+}
