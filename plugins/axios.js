c7ca5bfa9b7238e48ce701e59f76e8163210dab6
njzy <1120897196@qq.com>
2019-03-18 20:20:34 +0800

    feat: 增加Token和请求错误统一处理功能

---
+++ plugins/axios.js
@@ -0,0 +1,34 @@
+export default ({ app, store, $axios }) => {
+  $axios.onRequest(config => {
+    if (store.state.token) {
+      config.headers['Authorization'] = `Bearer ${store.state.token}`
+    }
+  })
+  $axios.onResponse(response => {
+    const data = response.data
+    if (!data.success && data.message) {
+      return app.$message({
+        message: data.message,
+        type: 'warning',
+        center: true
+      })
+    }
+  })
+  $axios.onError(err => {
+    const data = err.response.data
+    if (err.response.status == 404) {
+      return app.$message({
+        message: '404-接口不存在',
+        type: 'error',
+        center: true
+      })
+    }
+    if (!data.success && data.message) {
+      return app.$message({
+        message: data.message,
+        type: 'error',
+        center: true
+      })
+    }
+  })
+}
